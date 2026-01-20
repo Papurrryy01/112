41bb4f74be959a30007b63afe71d6f07513478cd
Carlos Vera <vera.0214@outlook.com>
2026-01-20 17:33:03 -0600

    113 1st assignment

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
+
+# Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, "posts/post_list.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "posts/post_detail.html", {"post": post})
